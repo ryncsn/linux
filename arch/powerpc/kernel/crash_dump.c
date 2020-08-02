0d7780fce8bf5d878d449453a49fea25f1fdb079
Kairui Song <kasong@redhat.com>
2020-08-02 22:35:53 +0800

    Implement copy_to_oldmem_page for ppc

--- arch/powerpc/kernel/crash_dump.c
+++ arch/powerpc/kernel/crash_dump.c
@@ -80,6 +80,19 @@ static size_t copy_oldmem_vaddr(void *vaddr, char *buf, size_t csize,
 	return csize;
 }
 
+static size_t copy_to_oldmem_vaddr(char *src, void *vaddr, size_t csize,
+                               unsigned long offset, int userbuf)
+{
+	if (userbuf) {
+		if (copy_from_user((vaddr + offset), (char __user *)src, csize))
+			return -EFAULT;
+	} else
+		memcpy((vaddr + offset), src, csize);
+
+	return csize;
+}
+
+
 /**
  * copy_oldmem_page - copy one page from "oldmem"
  * @pfn: page frame number to be copied
@@ -116,6 +129,44 @@ ssize_t copy_oldmem_page(unsigned long pfn, char *buf,
 
 	return csize;
 }
+
+/**
+ * copy_oldmem_page - copy one page from "oldmem"
+ * @pfn: page frame number to be copied
+ * @buf: target memory address for the copy; this can be in kernel address
+ *      space or user address space (see @userbuf)
+ * @csize: number of bytes to copy
+ * @offset: offset in bytes into the page (based on pfn) to begin the copy
+ * @userbuf: if set, @buf is in user address space, use copy_to_user(),
+ *      otherwise @buf is in kernel address space, use memcpy().
+ *
+ * Copy a page from "oldmem". For this page, there is no pte mapped
+ * in the current kernel. We stitch up a pte, similar to kmap_atomic.
+ */
+ssize_t copy_to_oldmem_page(unsigned long pfn, char *src, size_t csize,
+			    unsigned long offset, int userbuf)
+{
+	void  *vaddr;
+	phys_addr_t paddr;
+
+	if (!csize)
+		return 0;
+
+	csize = min_t(size_t, csize, PAGE_SIZE);
+	paddr = pfn << PAGE_SHIFT;
+
+	if (memblock_is_region_memory(paddr, csize)) {
+		vaddr = __va(paddr);
+		csize = copy_to_oldmem_vaddr(src, vaddr, csize, offset, userbuf);
+	} else {
+		vaddr = ioremap_cache(paddr, PAGE_SIZE);
+		csize = copy_to_oldmem_vaddr(src, vaddr, csize, offset, userbuf);
+		iounmap(vaddr);
+	}
+
+	return csize;
+}
+
 
 #ifdef CONFIG_PPC_RTAS
 /*
