ef6bf836dca050b243918b82de8d8e298e11d490
Kairui Song <kasong@tencent.com>
2025-05-02 03:22:34 +0800

    mm, swap: add a swap helper for bypassing only read ahead
    The swap cache now has a very low overhead, bypassing it is not helpful
    anymore. To prepare for unifying the swap in path, introduce a new
    helper that only bypasses read ahead and does not bypass the swap cache.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- mm/swap.h
+++ mm/swap.h
@@ -217,6 +217,7 @@ struct folio *swap_cluster_readahead(swp_entry_t entry, gfp_t flag,
 		struct mempolicy *mpol, pgoff_t ilx);
 struct folio *swapin_readahead(swp_entry_t entry, gfp_t flag,
 		struct vm_fault *vmf);
+struct folio *swapin_entry(swp_entry_t entry, struct folio *folio);
 void swap_update_readahead(struct folio *folio, struct vm_area_struct *vma,
 			   unsigned long addr);
 
@@ -299,6 +300,11 @@ static inline struct folio *swap_cluster_readahead(swp_entry_t entry,
 
 static inline struct folio *swapin_readahead(swp_entry_t swp, gfp_t gfp_mask,
 			struct vm_fault *vmf)
+{
+	return NULL;
+}
+
+static inline struct folio *swapin_entry(swp_entry_t ent, struct folio *folio)
 {
 	return NULL;
 }
