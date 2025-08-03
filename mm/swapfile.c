fc5387b2f20dc831cd32fdfd49910cf59c0abc85
Kairui Song <kasong@tencent.com>
2025-08-03 22:17:49 +0800

    mm, swap: remove no longer needed _swap_info_get
    There is now only two users of _swap_info_get after consolidating these
    callers, folio_try_reclaim_swap and swp_swapcount.
    
    folio_try_reclaim_swap holds the folio lock and the folio is in swap
    cache, _swap_info_get is redundant.
    
    For swp_swapcount, _swap_info_get is insufficient as the swap entry is
    no pinned so the device could be swapped off anytime, it should use
    get_swap_device instead.
    
    And after these change, _swap_info_get is no longer used, we can safely
    remove it.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1376,35 +1376,6 @@ void folio_put_swap(struct folio *folio, struct page *subpage)
 	swap_put_entries(swp_info(entry), swp_offset(entry), nr_pages);
 }
 
-static struct swap_info_struct *_swap_info_get(swp_entry_t entry)
-{
-	struct swap_info_struct *si;
-	unsigned long offset;
-
-	if (!entry.val)
-		goto out;
-	si = swp_get_info(entry);
-	if (!si)
-		goto bad_nofile;
-	if (data_race(!(si->flags & SWP_USED)))
-		goto bad_device;
-	offset = swp_offset(entry);
-	if (offset >= si->max)
-		goto bad_offset;
-	return si;
-
-bad_offset:
-	pr_err("%s: %s%08lx\n", __func__, Bad_offset, entry.val);
-	goto out;
-bad_device:
-	pr_err("%s: %s%08lx\n", __func__, Unused_file, entry.val);
-	goto out;
-bad_nofile:
-	pr_err("%s: %s%08lx\n", __func__, Bad_file, entry.val);
-out:
-	return NULL;
-}
-
 static unsigned char swap_put_entry_locked(struct swap_info_struct *si,
 					   struct swap_cluster_info *ci,
 					   swp_entry_t entry)
@@ -1561,7 +1532,7 @@ int swp_swapcount(swp_entry_t entry)
 	pgoff_t offset;
 	unsigned char *map;
 
-	si = _swap_info_get(entry);
+	si = get_swap_device(entry);
 	if (!si)
 		return 0;
 
@@ -1591,6 +1562,7 @@ int swp_swapcount(swp_entry_t entry)
 	} while (tmp_count & COUNT_CONTINUED);
 out:
 	swap_unlock_cluster(ci);
+	put_swap_device(si);
 	return count;
 }
 
@@ -1622,26 +1594,10 @@ unlock_out:
 	return ret;
 }
 
-static bool folio_swapped(struct folio *folio)
-{
-	swp_entry_t entry = folio->swap;
-	struct swap_info_struct *si = _swap_info_get(entry);
-
-	if (!si)
-		return false;
-
-	if (!IS_ENABLED(CONFIG_THP_SWAP) || likely(!folio_test_large(folio)))
-		return swap_entry_swapped(si, entry);
-
-	return swap_page_trans_huge_swapped(si, entry, folio_order(folio));
-}
-
 static bool folio_swapcache_freeable(struct folio *folio)
 {
 	VM_BUG_ON_FOLIO(!folio_test_locked(folio), folio);
 
-	if (!folio_test_swapcache(folio))
-		return false;
 	if (folio_test_writeback(folio))
 		return false;
 
@@ -1664,6 +1620,21 @@ static bool folio_swapcache_freeable(struct folio *folio)
 		return false;
 
 	return true;
+}
+
+static bool folio_swapped(struct folio *folio)
+{
+	swp_entry_t entry = folio->swap;
+	struct swap_info_struct *si;
+
+	VM_WARN_ON_ONCE_FOLIO(!folio_test_locked(folio), folio);
+	VM_WARN_ON_ONCE_FOLIO(!folio_test_swapcache(folio), folio);
+
+	si = swp_info(entry);
+	if (!IS_ENABLED(CONFIG_THP_SWAP) || likely(!folio_test_large(folio)))
+		return swap_entry_swapped(swp_info(entry), entry);
+
+	return swap_page_trans_huge_swapped(si, entry, folio_order(folio));
 }
 
 /**
