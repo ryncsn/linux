99f8c1b85a1d4a2d707f5a92d3851a9e11b28c0e
Kairui Song <kasong@tencent.com>
2025-11-22 01:42:29 +0800

    mm/gup: use new helpers for marking folios as referenced
    Marking the folio as referenced in gup fast was first introduced by commit
    8ee53820edfd ("thp: mmu_notifier_test_young"). Then later refractored
    but still basically the samething in commit e93480537fd7 ("mm/gup: Mark all
    pages PageReferenced in generic get_user_pages_fast()"). Some implementation
    may changed over days by the goal is the same: provide hotness info for better
    memory management.
    
    We have made all users that cares about a folio's hotness info to the
    new helper, so convert this updater too.
    
    Using folio_mark_access here to allow promotion might be OK, but that
    is slightly slower. To reduce overhead and keep the behaviour change
    minimal, use the cautious helper to only mark the folio as referenced.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- mm/gup.c
+++ mm/gup.c
@@ -2898,7 +2898,7 @@ static int gup_fast_pte_range(pmd_t pmd, pmd_t *pmdp, unsigned long addr,
 			gup_put_folio(folio, 1, flags);
 			goto pte_unmap;
 		}
-		folio_set_referenced(folio);
+		folio_mark_referenced(folio);
 		pages[*nr] = page;
 		(*nr)++;
 	} while (ptep++, addr += PAGE_SIZE, addr != end);
@@ -2967,7 +2967,7 @@ static int gup_fast_pmd_leaf(pmd_t orig, pmd_t *pmdp, unsigned long addr,
 	*nr += refs;
 	for (; refs; refs--)
 		*(pages++) = page++;
-	folio_set_referenced(folio);
+	folio_mark_referenced(folio);
 	return 1;
 }
 
@@ -3011,7 +3011,7 @@ static int gup_fast_pud_leaf(pud_t orig, pud_t *pudp, unsigned long addr,
 	*nr += refs;
 	for (; refs; refs--)
 		*(pages++) = page++;
-	folio_set_referenced(folio);
+	folio_mark_referenced(folio);
 	return 1;
 }
 
