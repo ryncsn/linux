50731c4c9d201074f856af569e838d46516c870b
Kairui Song <kasong@tencent.com>
2025-11-25 15:02:59 +0800

    mm/vmscan: conver to new bitwise identical helper
    No feature change, just prepare for removal of PG_referenced.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -899,7 +899,7 @@ static enum folio_references folio_check_references(struct folio *folio,
 		return folio_promote_lru_refs(folio) ? FOLIOREF_ACTIVATE : FOLIOREF_KEEP;
 	}
 
-	referenced_folio = folio_test_clear_referenced(folio);
+	referenced_folio = folio_test_clear_referenced_bit(folio);
 
 	if (referenced_ptes) {
 		/*
@@ -916,7 +916,7 @@ static enum folio_references folio_check_references(struct folio *folio,
 		 * so that recently deactivated but used folios are
 		 * quickly recovered.
 		 */
-		folio_set_referenced(folio);
+		folio_mark_referenced_by_bit(folio);
 
 		if (referenced_folio || referenced_ptes > 1)
 			return FOLIOREF_ACTIVATE;
