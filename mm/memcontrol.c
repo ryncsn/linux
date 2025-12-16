8d1148a484a4ab53bf4cbf5e32fe7fcc5f4bc9e2
Kairui Song <kasong@tencent.com>
2025-12-16 17:27:21 +0800

    mm/memcontrol: allow update of LRU statistic without holding LRU lock
    To enable moving file pages in a  in folio_mark_accessed
    The check was added by commit ca707239e8a7 to avoid loop of the
    shrinker, this can be easily resolved by checking at read time instead,
    which have a smaller overhead.
    
    Signed-off-by: Kairui Song <kasong@tencent.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1266,8 +1266,9 @@ struct lruvec *folio_lruvec_lock_irqsave(struct folio *folio,
  * @zid: zone id of the accounted pages
  * @nr_pages: positive when adding or negative when removing
  *
- * This function must be called under lru_lock, just before a page is added
- * to or just after a page is removed from an lru list.
+ * This function must be called before a page is added * to or just after
+ * a page is removed from an lru list. Caller need to protect the lruvec
+ * with lock or RCU.
  */
 void mem_cgroup_update_lru_size(struct lruvec *lruvec, enum lru_list lru,
 				int zid, int nr_pages)
